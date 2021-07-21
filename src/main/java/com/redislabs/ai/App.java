67505725225acee4b8a70b58c530796a05d53d3e
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-07-21 11:48:35 -0700

    mvn simple artifact generated skeleton

---
+++ src/main/java/com/redislabs/ai/App.java
@@ -0,0 +1,13 @@
+package com.redislabs.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
